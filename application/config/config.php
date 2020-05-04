6c50ec697707896c94b8ffb36c5998bad633a6f9
Sigit Suryono <sigit_suryono25@hotmail.com>
2020-05-04 13:06:12 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url']    = "http://" . $_SERVER['HTTP_HOST'] .
-    str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
+$protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+$host = $_SERVER['HTTP_HOST'];
+$host .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
+$config['base_url'] = $protocol . $host;
 
 /*
 |--------------------------------------------------------------------------

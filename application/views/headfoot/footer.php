1f59a2b9c8288a7937dcd231b9839d1c7b992fbb
Sigit Suryono <sigit_suryono25@hotmail.com>
2020-05-07 21:38:57 +0700

    Update footer.php

--- application/views/headfoot/footer.php
+++ application/views/headfoot/footer.php
@@ -28,6 +28,27 @@
              statusChangeCallback(response);
            });
          }
+  
+         function statusChangeCallback(response) {
+                console.log('statusChangeCallback');
+                console.log(response);
+                // The response object is returned with a status field that lets the
+                // app know the current login status of the person.
+                // Full docs on the response object can be found in the documentation
+                // for FB.getLoginStatus().
+                if (response.status === 'connected') {
+                    // Logged into your app and Facebook.
+                    console.log('Welcome!  Fetching your information.... ');
+                    FB.api('/me', function (response) {
+                        console.log('Successful login for: ' + response.name);
+//                         document.getElementById('status').innerHTML =
+//                           'Thanks for logging in, ' + response.name + '!';
+                    });
+                } else {
+                    // The person is not logged into your app or we are unable to tell.
+                   console.log('Please log into this app.');
+                }
+            }
 </script>
 <script>
     $(document).ready(function () {

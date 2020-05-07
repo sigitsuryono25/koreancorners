5254eba56d32efd3b9f73f410338d94a60efa728
Sigit Suryono <sigit_suryono25@hotmail.com>
2020-05-07 21:35:27 +0700

    Update footer.php

--- application/views/headfoot/footer.php
+++ application/views/headfoot/footer.php
@@ -12,8 +12,6 @@
              version    : 'v7.0'
            });
         
-      FB.AppEvents.logPageView();   
-
          (function(d, s, id){
             var js, fjs = d.getElementsByTagName(s)[0];
             if (d.getElementById(id)) {return;}

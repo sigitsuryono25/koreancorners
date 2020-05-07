fd530044712538170ff2936f7a94c0eb32215d99
Sigit Suryono <sigit_suryono25@hotmail.com>
2020-05-07 21:15:02 +0700

    Update footer.php

--- application/views/headfoot/footer.php
+++ application/views/headfoot/footer.php
@@ -4,25 +4,11 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" ></script>
 
 <script>
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '296791887385267',
-      cookie     : true,
-      xfbml      : true,
-      version    : 'v7.0'
+  $(document).ready(function () {
+    FB.getLoginStatus(function(response) {
+        statusChangeCallback(response);
     });
-      
-    FB.AppEvents.logPageView();   
-      
-  };
-
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "https://connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
+  });
 </script>
 <script>
     $(document).ready(function () {
